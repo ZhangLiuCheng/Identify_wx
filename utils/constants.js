37fde401354c4330f7cc8279d46c6e2ae51253ed
liucheng zhang <414616504@qq.com.com>
2021-03-25 13:03:29 +0800

    update

--- utils/constants.js
+++ utils/constants.js
@@ -2,7 +2,6 @@
 var host = 'https://identify.xbbvip.com/v1/'
 var hostFill = 'https://fill.xbbvip.com/v3/'
 
-
 module.exports = {
   identifyFile: host + 'identifyFile',
   queryMore: host + 'queryMore',
